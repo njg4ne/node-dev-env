fb03727e2d7bb108c6b4116a04dbcb50148fc2d4
Nicholas-D-W-Y <njg4ne@virginia.edu>
2022-05-17 18:45:06 +0000

    allowing command line argument for key name rather than hardcoding

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ RUN apt install git -y
 USER node
 ENV HOME /home/node
 WORKDIR $HOME
+ENTRYPOINT ["bash", "-c", "cp gitfile .gitconfig;. auth.sh secrets/$0;$SHELL"] 
--- shorthand.sh
+++ shorthand.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
+# new
 alias c=clear;
+dcRun() { docker-compose run $1-service $2; };
+
+# deprecated
 alias dcUp="docker-compose up -d";
 alias dcDown="docker-compose down";
 alias dPrune="docker system prune --force";
 alias dcKill="docker-compose kill";
-alias dcRestart="dcDown; dcUp; dcIn $1;";
-alias dcBuild="docker-compose build"
-alias nodeline="docker-compose run dev-service"
+alias dcBuild="docker-compose build";
 
 dcIn() { docker-compose exec $1-service bash; };
 
+# confirmation
 echo "Shorthands Added!";
